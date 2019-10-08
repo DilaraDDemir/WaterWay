f37519ce27b0e560f9e3c7a259a0639ceb7631d1
Dilara Demir <dilarademir95@gmail.com>
2019-10-08 23:01:00 +0300

    WaterWayProject

---
+++ WaterWay/deneme-entity/frmBayiGiris.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace deneme_entity
+{
+    partial class frmBayiGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBayiGiris));
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.txtParola = new System.Windows.Forms.TextBox();
+            this.btnKapat = new System.Windows.Forms.Button();
+            this.btnGeri = new System.Windows.Forms.Button();
+            this.lblSifre = new System.Windows.Forms.Label();
+            this.lblKullaniciAdi = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Location = new System.Drawing.Point(564, 324);
+            this.btnGiris.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(116, 42);
+            this.btnGiris.TabIndex = 0;
+            this.btnGiris.Text = "Giris";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.Click += new System.EventHandler(this.BtnGiris_Click);
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(539, 224);
+            this.txtKullaniciAdi.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(170, 36);
+            this.txtKullaniciAdi.TabIndex = 1;
+            // 
+            // txtParola
+            // 
+            this.txtParola.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.txtParola.Location = new System.Drawing.Point(541, 267);
+            this.txtParola.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtParola.Name = "txtParola";
+            this.txtParola.PasswordChar = '*';
+            this.txtParola.Size = new System.Drawing.Size(168, 36);
+            this.txtParola.TabIndex = 1;
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnKapat.BackgroundImage")));
+            this.btnKapat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnKapat.Location = new System.Drawing.Point(1097, 12);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(56, 53);
+            this.btnKapat.TabIndex = 18;
+            this.btnKapat.UseVisualStyleBackColor = true;
+            this.btnKapat.Click += new System.EventHandler(this.BtnKapat_Click);
+            // 
+            // btnGeri
+            // 
+            this.btnGeri.BackColor = System.Drawing.SystemColors.Control;
+            this.btnGeri.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnGeri.BackgroundImage")));
+            this.btnGeri.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnGeri.Location = new System.Drawing.Point(12, 12);
+            this.btnGeri.Name = "btnGeri";
+            this.btnGeri.Size = new System.Drawing.Size(124, 53);
+            this.btnGeri.TabIndex = 17;
+            this.btnGeri.UseVisualStyleBackColor = false;
+            this.btnGeri.Click += new System.EventHandler(this.BtnGeri_Click);
+            // 
+            // lblSifre
+            // 
+            this.lblSifre.AutoSize = true;
+            this.lblSifre.BackColor = System.Drawing.Color.Transparent;
+            this.lblSifre.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSifre.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblSifre.Location = new System.Drawing.Point(355, 269);
+            this.lblSifre.Name = "lblSifre";
+            this.lblSifre.Size = new System.Drawing.Size(70, 29);
+            this.lblSifre.TabIndex = 31;
+            this.lblSifre.Text = "Şifre";
+            // 
+            // lblKullaniciAdi
+            // 
+            this.lblKullaniciAdi.AutoSize = true;
+            this.lblKullaniciAdi.BackColor = System.Drawing.Color.Transparent;
+            this.lblKullaniciAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKullaniciAdi.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblKullaniciAdi.Location = new System.Drawing.Point(355, 224);
+            this.lblKullaniciAdi.Name = "lblKullaniciAdi";
+            this.lblKullaniciAdi.Size = new System.Drawing.Size(166, 29);
+            this.lblKullaniciAdi.TabIndex = 30;
+            this.lblKullaniciAdi.Text = "Kullanıcı Adı";
+            // 
+            // frmBayiGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::deneme_entity.Properties.Resources.istockphoto_486846754_612x612;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1165, 645);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblSifre);
+            this.Controls.Add(this.lblKullaniciAdi);
+            this.Controls.Add(this.btnKapat);
+            this.Controls.Add(this.btnGeri);
+            this.Controls.Add(this.txtParola);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Controls.Add(this.btnGiris);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "frmBayiGiris";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.TextBox txtParola;
+        private System.Windows.Forms.Button btnKapat;
+        private System.Windows.Forms.Button btnGeri;
+        private System.Windows.Forms.Label lblSifre;
+        private System.Windows.Forms.Label lblKullaniciAdi;
+    }
+}
+
